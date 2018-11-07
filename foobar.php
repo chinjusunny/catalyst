3efcd11e97e2cafb164fda681f9353d43cb04577
Chinju Sunny <chinju_sunny@yahoo.com>
2018-11-08 09:38:20 +1100

    Update foobar.php

--- foobar.php
+++ foobar.php
@@ -1,3 +1,21 @@
 <?php
-
+for($i=0;$i<=100;$i++)
+{
+  if($i%3==0 && $i%5==0)
+  {
+    echo "foobar\n";
+  }
+  else if($i%3==0)
+  {
+    echo "foo\n";
+  }
+  else if($i%5==0)
+  {
+    echo "bar\n";
+  }
+  else
+  {
+    echo $i."\n";
+  }
+}
 ?>
