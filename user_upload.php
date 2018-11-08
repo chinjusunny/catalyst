3d567ce786ab48a8f7e109d6cac86b58ab74c927
Chinju Sunny <chinju_sunny@yahoo.com>
2018-11-08 14:51:31 +1100

    Update user_upload.php

--- user_upload.php
+++ user_upload.php
@@ -5,5 +5,18 @@ $p=""; // Mysql password
 $db_name="Users"; // Database name
 $con=mysql_connect($h, $u, $p);
 mysql_select_db($db_name, $con);
+if(! $con) 
+{
+    die('Could not connect: ' . mysql_error());
+}
+
+$sql=" CREATE TABLE users(".
+      "name VARCHAR(100) NOT NULL,".
+       "surname VARCHAR(100) NOT NULL,".
+       "email VARCHAR(255) NOT NULL,".
+       "PRIMARY KEY (email));";
+    
+
+
 
 ?>
